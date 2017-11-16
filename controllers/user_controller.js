7472f5908f097ea5280acf29dadf7f214117d2f0
Joselon <jose.navarro.osta@gmail.com>
2017-11-16 22:31:16 +0100

    Sec

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,11 +1,5 @@
 var users= {admin: {id:1, username:"Joselon79", password:"Patata1"},
-	    pepe: {id:2, username:"Yao", password:"5678"},
-	    Ambrus: {id:3, username:"El Chúngaro",password:"Horváth"},
-	Dunch:{id:4,username:"Kurare",password:"contrasenya"},
-	juarmix: {id:5, username:"JuarMix", password:"123abc.17._@"},
-	depositos: {id:6, username:"Depósito", password:"online17"},
-	sanidad: {id:7, username:"Usuario Sanidad Oesía", password:"sanidad17"},
- orangepruebas: {id:8, username:"Usuario Orange Pruebas", password:"orange123"}
+	    pepe: {id:2, username:"Yao", password:"5678"}
 	   };
 
 // Comprueba si el usuario esta registrado en users
