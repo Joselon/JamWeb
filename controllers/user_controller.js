afa23ac66910ffe2b280b5f2c76e428ce341c93f
Joselon <jose.navarro.osta@gmail.com>
2018-04-06 22:07:24 +0200

    Moderdonia

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,6 +1,7 @@
 var users= {admin: {id:1, username:"Joselon79", password:"Patata1"},
 	    pepe: {id:2, username:"Yao", password:"5678"},
-kogoyo: {id:2, username:"Kogoyo", password:"1234"}
+kogoyo: {id:3, username:"Kogoyo", password:"1234"},
+Moderdonia: {id:4, username:"Moderdonia", password:"LVMx2017"}
 	   };
 
 // Comprueba si el usuario esta registrado en users
