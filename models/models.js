bddf3be31be318fe82983d881c42f9ad43994404
Joselon <jose.navarro.osta@gmail.com>
2023-07-07 19:26:47 +0100

    Pendiente url Databaser regexp

--- models/models.js
+++ models/models.js
@@ -1,59 +1,67 @@
-var path=require('path');
+var path = require('path');
 
 // Postgres DATABASE_URL = postgres://user:passwd@host:port/database
 // SQLite DATABASE_URL = sqlite://:@:/
+//mysql://user:pass@host/database?reconnect=true
 var url = process.env.CLEARDB_DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
-console.log("URL: " +url+ "de: "+ process.env.CLEARDB_DATABASE_URL);
-var DB_name	=	(url[6]||null);
-var user	=	(url[2]||null);
-var pwd		=	(url[3]||null);
-var protocol	=	(url[1]||null);
-var dialect	=	(url[1]||null);
-var port	=	(url[5]||null);
-var host	=	(url[4]||null);
-var storage	= process.env.DATABASE_STORAGE;
+//console.log("URL: " +url+ " de: "+ process.env.CLEARDB_DATABASE_URL);
+if (url != null) {
+	var DB_name = (url[6] || null);
+	var user = (url[2] || null);
+	var pwd = (url[3] || null);
+	var protocol = (url[1] || null);
+	var dialect = (url[1] || null);
+	var port = (url[5] || null);
+	var host = (url[4] || null);
+}
+var storage = process.env.DATABASE_STORAGE;
 
 // Cargar Modelo ORM
-var Sequelize=require('sequelize');
+var Sequelize = require('sequelize');
 
 // Usar BBDD SQLite o Postgres
 
-var sequelize= new Sequelize(DB_name,user,pwd,
-			{dialect:protocol,
-			 protocol:protocol,
-			 port:port,
-			 host:host,
-			 storage:storage, // solo SQLite (.env)
-			 omitNull:true    // solo Postgres
-			}
-		  );
+var sequelize = new Sequelize(DB_name, user, pwd,
+	{
+		dialect: protocol,
+		protocol: protocol,
+		port: port,
+		host: host,
+		storage: storage, // solo SQLite (.env)
+		omitNull: true    // solo Postgres
+	}
+);
 
 // Importar la definicion de la tabla Quiz en quiz.js
-var quiz_path=path.join(__dirname,'quiz');
-var Quiz= sequelize.import(quiz_path);
+var quiz_path = path.join(__dirname, 'quiz');
+var Quiz = sequelize.import(quiz_path);
 
 //Imiportar definicion de la tabla Commnet
-var comment_path=path.join(__dirname,'comment');
-var Comment=sequelize.import(comment_path);
+var comment_path = path.join(__dirname, 'comment');
+var Comment = sequelize.import(comment_path);
 
 Comment.belongsTo(Quiz);
 Quiz.hasMany(Comment);
 
-exports.Quiz=Quiz; // exportar definicion de tabla Quiz
-exports.Comment=Comment; //exportamos definicion de tabla Comment
+exports.Quiz = Quiz; // exportar definicion de tabla Quiz
+exports.Comment = Comment; //exportamos definicion de tabla Comment
 
 // sequelize.sync() crea e inicializa tabla de preguntas de DB
-sequelize.sync().then(function() {
- // then(..) ejecuta el manejador una vez creada la tabla
-  Quiz.count().then(function (count){
-   if(count==0) { // la tabla se inicia solo si esta vacia
-     Quiz.create({ pregunta: 'Capital de Italia',
-		   respuesta: 'Roma',
-		   tema: 'humanidades'});
-     Quiz.create({ pregunta: 'Capital de Portugal',
-		   respuesta: 'Lisboa',
-		   tema: 'humanidades'})
-   .then(function(){console.log('Base de flatos inicial izada')});
-  };
- });
+sequelize.sync().then(function () {
+	// then(..) ejecuta el manejador una vez creada la tabla
+	Quiz.count().then(function (count) {
+		if (count == 0) { // la tabla se inicia solo si esta vacia
+			Quiz.create({
+				pregunta: 'Capital de Italia',
+				respuesta: 'Roma',
+				tema: 'humanidades'
+			});
+			Quiz.create({
+				pregunta: 'Capital de Portugal',
+				respuesta: 'Lisboa',
+				tema: 'humanidades'
+			})
+				.then(function () { console.log('Base de flatos inicial izada') });
+		};
+	});
 });
