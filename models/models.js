c6fb689e2ef6b3ea006f4f23309084c29872e8c8
Joselon <jose.navarro.osta@gmail.com>
2023-07-11 10:02:10 +0100

    mysql RegExp

--- models/models.js
+++ models/models.js
@@ -1,20 +1,29 @@
 var path = require('path');
 
-// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+
 // SQLite DATABASE_URL = sqlite://:@:/
-//mysql://user:pass@host/database?reconnect=true
-var url = process.env.CLEARDB_DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
-//console.log("URL: " +url+ " de: "+ process.env.CLEARDB_DATABASE_URL);
-if (url != null) {
-	var DB_name = (url[6] || null);
-	var user = (url[2] || null);
-	var pwd = (url[3] || null);
-	var protocol = (url[1] || null);
-	var dialect = (url[1] || null);
-	var port = (url[5] || null);
-	var host = (url[4] || null);
-}
-var storage = process.env.DATABASE_STORAGE;
+
+
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+/*var url = process.env.CLEARDB_DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var protocol = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);*/
+
+//CLEARDB_DATABASE_URL = mysql://user:pass@host/database?reconnect=true
+var url = process.env.CLEARDB_DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\/(.*)\?(.*)/);
+var DB_name = (url[6] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
+
+var storage = process.env.CLEARDB_DATABASE_STORAGE;
 
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
